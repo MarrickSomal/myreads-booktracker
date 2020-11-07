b8c7354f51ff1a49741084b6fe051e89428864e0
Marrick Somal <marrick.somal@gmail.com>
2020-11-07 12:19:20 +0000

    feat: search books without a thumbnail or author
    conditional ternary operators added in className "book-cover" and "book-author".
    these operators allow books to be rendered which do not contain either thumbnail or author data

--- src/App.js
+++ src/App.js
@@ -94,10 +94,12 @@ class BooksApp extends Component {
               <li key={book.id}>
               <div className="book">
               <div className='book-top'>
-              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                {/* ternary conditional added below: if imageLinks data is empty for a book then show nothing, otherwise show the thumbnail value   */}
+              <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: ((book.imageLinks) ? `url(${book.imageLinks.thumbnail})` : "") }}></div>
               </div>
               <div className="book-title">{book.title}</div>
-              <div className="book-authors">{book.authors}</div>
+              {/* ternary conditional added below: if authors data is empty for a book then show nothing, otherwise show the authors value   */}
+              <div className="book-authors">{((book.authors) ? book.authors : "")}</div>
               </div>
               </li>
               ))}
