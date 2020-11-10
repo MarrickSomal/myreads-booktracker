6982fb24080d543009eefaee0ea10a81e0ac921f
Marrick Somal <marrick.somal@gmail.com>
2020-11-10 17:41:22 +0000

    feat: split code into components (MyReads page)
    - Bookshelves array created and associated old code removed
    - Code moved from App.js to ListBooks and BookShelf components

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -3,7 +3,16 @@ import React, { Component } from 'react'
 
 class BookShelf extends Component {
 
-
+    render() {
+        return (
+            <div className="bookshelf">
+              <h2 className="bookshelf-title">Shelf Name</h2>
+              <div className="bookshelf-books">
+                <ol className="books-grid">
+                </ol>
+              </div>
+            </div>
+        )
+    }
 }
-
 export default BookShelf;
